c455b4e6810bf9937a5734e0348035731c5daafc
Brian Diaz <bidiaz101@gmail.com>
2021-10-16 13:50:42 -0500

    commit tree 27cb1fc804621fed249c312a861f2de0a1f66aa9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+    name_arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speaker_arr)
+    speaker_arr.map.with_index(1) {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+end
+
+def printer(name_arr)
+    batch_badge_creator(name_arr).each {|badge| puts badge}
+    assign_rooms(name_arr).each {|assignment| puts assignment}
+end
